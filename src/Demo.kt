9f62b38c763c4de2ffc695d31c180b10590f3e65
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-03-14 11:31:12 -0700

    Uso de Git
    no me gusta git, me caen gordos los copiones, por eso el profe nos obliga a usar git

---
+++ src/Demo.kt
@@ -0,0 +1,16 @@
+/**
+ * CETis108
+ * Semestre: Febrero-Julio 2022
+ * Asignatura: Desarrolla aplicaciones móviles
+ * Profesor: Bidkar Aragón Cárdenas
+ * Alumno(a): ?
+ * Fecha: 14/03/22 11:28
+ * Grupo: 6AM-PR Bloque A
+ */
+
+
+fun main() {
+    println("Hola Git y GitHub")
+    println("Adios Git y GitHub")
+    println("Hola de nuevo")
+}
