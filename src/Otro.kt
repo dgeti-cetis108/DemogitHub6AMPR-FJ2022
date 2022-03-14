e0252ca5fb1d19cf5105791bf39bd41008f0f9b8
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-03-14 11:51:16 -0700

    Clase de 14/03/2022
    Hola mundo con variables

---
+++ src/Otro.kt
@@ -0,0 +1,15 @@
+/**
+ * CETis108
+ * Semestre: Febrero-Julio 2022
+ * Asignatura: Desarrolla aplicaciones móviles
+ * Profesor: Bidkar Aragón Cárdenas
+ * Alumno(a): ?
+ * Fecha: 14/03/22 11:46
+ * Grupo: 6AM-PR Bloque A
+ */
+
+
+fun main() {
+    val nombre = pedirCadenaPorTeclado("Como te llamas?")
+    println("Hola $nombre")
+}
